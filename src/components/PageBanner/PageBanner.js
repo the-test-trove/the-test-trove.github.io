5db5599cf052f8b17d56abd76bdddeccdb812ff1
Adithya Krishna <aadithya794@gmail.com>
2022-01-17 16:50:55 +0530

    Updated Dates
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- src/components/PageBanner/PageBanner.js
+++ src/components/PageBanner/PageBanner.js
@@ -1,4 +1,4 @@
-import React, { version } from 'react'
+import React from 'react'
 import { Helmet } from 'react-helmet'
 import { versions } from '../Constants/Constants';
 
@@ -9,11 +9,15 @@ export default function PageBanner() {
                 <div className="extra-container ">
                     <div className="pagebanner-1__tittle ">
                         <h1 class="pagebanner-1__tittle__webdesign schedule-tittle-eff">The Test Trove Conference</h1>
-                        {versions.map((version) => {
+                        {/* {versions.map((version) => {
                             return(
-                                <h1 class="pagebanner-1__tittle__conference schedule-tittle-eff2">{version.quarter}{" "}{version.year}</h1>
+                                <h4 class="pagebanner-1__tittle__conference schedule-tittle-eff2">{version.quarter}{" "}{version.year}</h4>
                             );
-                        })}
+                        })} */}
+                        <h2 class="pagebanner-2__tittle__conference">Call For Proposals Open</h2>
+                        <a href="https://cfp.devconf.info/" class="header__buy-ticket " target="_blank" without rel="noreferrer">
+                            <span>Proposals</span>
+                        </a>
                     </div>
                 </div>
             </div>
